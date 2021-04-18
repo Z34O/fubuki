226dc8295a8480c6b007bb7ac3d6f55ca3848cfa
kairo <itokairo.zc@gmail.com>
2021-04-18 10:35:50 +0800

    bug fixes

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import json
 import os
 import re
 
-# Output banner
-os.system("bash banner.sh")
-
 # Env variables available to all
 RED = "\033[31m"
 BLUE = "\033[34m"
@@ -49,7 +46,8 @@ def messages():
             os.chdir("..")
             break
         query = [token.strip() for token in unsanitized_query if token != '']
-        print(query)
+        if query == []:
+            continue
         for count, token in enumerate(query):
             next = query[count + 1] if count != len(query)-1 else None
             if token == "search":
@@ -68,7 +66,11 @@ def messages():
                 threadtype = next
 
         # Go to chosen thread type, iterate through directories and look for matches in message_1.json
-        os.chdir(threadtype)
+        try:
+            os.chdir(threadtype)
+        except FileNotFoundError:
+            print(f"{RED}Thread type not found!{WHITE}")
+            break
         for conversation in os.listdir():
             try:
                 os.chdir(conversation)
@@ -83,7 +85,7 @@ def messages():
                                       datetime.fromtimestamp(int(message["timestamp_ms"]) / 1000.0) > datefrom,
                                       datetime.fromtimestamp(int(message["timestamp_ms"]) / 1000.0) < dateuntil]
                         if all(conditions):
-                            result.append(message)
+                            result.append((message, data))
                             beautify(message, data)
                     except IndexError:
                         break # Go to next conversation if last message reached
@@ -153,7 +155,7 @@ def comments():
                               datetime.fromtimestamp(int(comment["timestamp"]) / 1000.0) > datefrom,
                               datetime.fromtimestamp(int(comment["timestamp"]) / 1000.0) < dateuntil]
                 if all(conditions):
-                    result.append(comment)
+                    result.append((comment, data["comments"][commentcount]))
                     beautify(comment, data["comments"][commentcount])
             except IndexError:
                 break # Stop if last comment is reached
@@ -161,28 +163,89 @@ def comments():
                 pass # If no content is found on comment, skip
             commentcount += 1
 
-# Starting point
-while True:
-    select = input("[fubuki: v.0.2b] ").split(" ")
+def posts():
+    os.chdir("posts")
 
-    if select[0] == "exit":
-        exit()
-    elif select[0] == "init":
-        datadirectory = select[1]
-        try:
-            os.chdir("../"+datadirectory)
-        except FileNotFoundError:
-            print(f"{RED}No facebook data file found!{WHITE}")
-    elif select[0] == "use":
-        try:
-            if select[1] == "messages":
-                messages()
-            elif select[1] == "comments":
-                comments()
-        except FileNotFoundError:
-            print(f"{RED}No file data found for your chosen module")
-            print(f"{BLUE}TIP: {ORANGE}Make sure you initialized your facebook information directory by 'init <directory>'{WHITE}")
-        except IndexError:
-            print(f"{RED}Command 'use' needs 1 valid argument!{WHITE}")
-    else:
-        print(f"{RED}'{select[0]}' is not a valid command!{WHITE}")
+    def beautify(data, post):
+        print(data["title"])
+        print("Post Status: "+post["post"])
+        postdate = datetime.fromtimestamp(int(data["timestamp"]) / 1000.0)
+        print(f"{postdate.month}/{postdate.day}/{postdate.year} {postdate.hour}:{postdate.minute}\n")
+
+    while True:
+        result = []
+
+        searchkey = r"."
+        datefrom = datetime.strptime("1/1/1969 00:00", dt_format)
+        dateuntil = datetime.now()
+
+        keywords = ["search", "from", "until"]
+
+        unsanitized_query = re.split("("+"|".join(keywords)+")", input(f"{BLUE}[posts] {ORANGE}"))
+        query = [token.strip() for token in unsanitized_query if token != '']
+        print(f"{WHITE}", end="")
+        if unsanitized_query[0] == "exit":
+            os.chdir("..")
+            break
+
+        for count, token in enumerate(query):
+            next = query[count + 1] if count != len(query)-1 else None
+            if token == "search":
+                searchkey = next
+            elif token == "from":
+                next += " 00:00" if ":" not in next else ""
+                datefrom = datetime.strptime(next, dt_format)
+            elif token == "until":
+                next += " 00:00" if ":" not in next else ""
+                dateuntil = datetime.strptime(next, dt_format)
+
+        data = json.load(open("your_posts_1.json", "r"))
+        postcount = 0
+
+        while True:
+            try:
+                post = data[postcount]["data"][0]
+                conditions = [bool(re.search(searchkey, post["post"])),
+                              bool(re.search(searchkey, data[postcount]["title"])),
+                              datetime.fromtimestamp(int(data[postcount]["timestamp"]) / 1000.0) > datefrom,
+                              datetime.fromtimestamp(int(data[postcount]["timestamp"]) / 1000.0) < dateuntil]
+                if all(conditions):
+                    result.append((data, post))
+                    beautify(data[postcount], post)
+            except IndexError:
+                break
+            except KeyError:
+                pass
+            postcount += 1
+
+# Starting point for REPL
+# If not used as REPL, make sure you are inside the data directory
+if __name__ == '__main__':
+    os.system("bash banner.sh")
+
+    while True:
+        select = input("[fubuki: v.0.2b] ").split(" ")
+
+        if select[0] == "exit":
+            exit()
+        elif select[0] == "init":
+            datadirectory = select[1]
+            try:
+                os.chdir("../"+datadirectory)
+            except FileNotFoundError:
+                print(f"{RED}No facebook data file found!{WHITE}")
+        elif select[0] == "use":
+            try:
+                if select[1] == "messages":
+                    messages()
+                elif select[1] == "comments":
+                    comments()
+                elif select[1] == "posts":
+                    posts()
+            except FileNotFoundError:
+                print(f"{RED}No file data found for your chosen module")
+                print(f"{BLUE}TIP: {ORANGE}Make sure you initialized your facebook information directory by 'init <directory>'{WHITE}")
+            except IndexError:
+                print(f"{RED}Command 'use' needs 1 valid argument!{WHITE}")
+        else:
+            print(f"{RED}'{select[0]}' is not a valid command!{WHITE}")
